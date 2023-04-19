4b4d7dbf833799d4d1764e89fc6bc09a2187084f
AlexBir1 <alexbir2003ggg@gmail.com>
2023-04-19 20:46:38 +0300

    SignalR

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static HubConnection hubConnection;
+
+        static async Task Main(string[] args)
+        {
+            hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:44313/notification").Build();
+            Console.WriteLine("Hello World!");
+
+            hubConnection.On<string>("Send", message => Console.WriteLine($"Msg: {message}"));
+            await hubConnection.StartAsync();
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+                if (message != "exit")
+                {
+                    await hubConnection.SendAsync("SendMessage", message);
+                }
+                else
+                    isExit = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SignalrPractice/SignalrPractice/Hubs/NotificationHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrPractice.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public Task SendMessage(string message)
+        {
+            var Result = CheckMessage(message);
+            return Clients.Others.SendAsync("Send", Result);
+        }
+
+        string CheckMessage(string _message)
+        {
+            if (_message.Contains("fuck"))
+                return "<forbidden_word>";
+            return _message;
+        }
+    }
+}
